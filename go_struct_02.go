15c04054d8e7519fbb36b9f0c7039cfc1509ed52
root <root@DESKTOP-MES1GMG.localdomain>
2020-12-01 16:27:03 +0900

    2번째 파일 구조체1

---
+++ go_struct_02.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+//사용자 정의 => type를 쓴 함수 생성
+type subscriber struct {
+	name   string
+	rate   int
+	active bool
+}
+
+func printInfo(s subscriber) {
+	fmt.Println("Name : ", s.name)
+	fmt.Println("Monthly rate : ", s.rate)
+	fmt.Println("Active? ", s.active)
+}
+
+func defaultSubscriber(name string) subscriber {
+	var s subscriber
+	s.name = name
+	s.rate = 5000
+	s.active = true
+	return s
+}
+
+func main() {
+	//가입중이던 구독자
+	s1 := defaultSubscriber("cho")
+	s1.rate = 4500
+	printInfo(s1)
+
+	//오늘 가입한 구독자
+	s2 := defaultSubscriber("park")
+	printInfo(s2)
+}
